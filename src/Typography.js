9d86b090d623b1d96892e8596410856657e901b4
Robbie Carlton <robbie.carlton@gmail.com>
2017-02-16 16:55:52 -0700

    (WIP) Typography page layout

--- src/Typography.js
+++ src/Typography.js
@@ -3,10 +3,109 @@ import styles from './Typography.css'
 import CSSModules from 'react-css-modules'
 
 function Typography (props) {
-  return <div styleName='big-red'>
-    Big red
-    <div styleName='little-black'>
-      little black
+  return <div styleName='page'>
+    <div styleName='title'>Typography</div>
+    <div styleName='section-title'>Headers</div>
+    <div styleName='section'>
+      <div styleName='style-card-row'>
+        <div styleName='style-card'>
+          <div styleName='style-name'>
+            Display
+          </div>
+          <div styleName='style-attributes'>
+            Circular Bold / 32pt, -0.5 ch, 36 line / Color: 2C4059
+          </div>
+          <div className='display'>
+            Five quacking zephyrs jolt my wax bed.
+          </div>
+        </div>
+        <div styleName='style-card'>
+          <div styleName='style-name'>
+            Display
+          </div>
+          <div styleName='style-attributes'>
+            Circular Bold / 32pt, -0.5 ch, 36 line / Color: 2C4059
+          </div>
+          <div className='display'>
+            Five quacking zephyrs jolt my wax bed.
+          </div>
+        </div>
+      </div>
+      <div styleName='style-card-row'>
+        <div styleName='style-card'>
+          <div styleName='style-name'>
+            Display
+          </div>
+          <div styleName='style-attributes'>
+            Circular Bold / 32pt, -0.5 ch, 36 line / Color: 2C4059
+          </div>
+          <div className='display'>
+            Five quacking zephyrs jolt my wax bed.
+          </div>
+        </div>
+        <div styleName='style-card'>
+          <div styleName='style-name'>
+            Display
+          </div>
+          <div styleName='style-attributes'>
+            Circular Bold / 32pt, -0.5 ch, 36 line / Color: 2C4059
+          </div>
+          <div className='display'>
+            Five quacking zephyrs jolt my wax bed.
+          </div>
+        </div>
+      </div>
+    </div>
+    <div styleName='section-title'>Body Styles</div>
+    <div styleName='section'>
+      <div styleName='style-card-row'>
+        <div styleName='style-card'>
+          <div styleName='style-name'>
+            Display
+          </div>
+          <div styleName='style-attributes'>
+            Circular Bold / 32pt, -0.5 ch, 36 line / Color: 2C4059
+          </div>
+          <div className='display'>
+            Five quacking zephyrs jolt my wax bed.
+          </div>
+        </div>
+        <div styleName='style-card'>
+          <div styleName='style-name'>
+            Display
+          </div>
+          <div styleName='style-attributes'>
+            Circular Bold / 32pt, -0.5 ch, 36 line / Color: 2C4059
+          </div>
+          <div className='display'>
+            Five quacking zephyrs jolt my wax bed.
+          </div>
+        </div>
+      </div>
+      <div styleName='style-card-row'>
+        <div styleName='style-card'>
+          <div styleName='style-name'>
+            Display
+          </div>
+          <div styleName='style-attributes'>
+            Circular Bold / 32pt, -0.5 ch, 36 line / Color: 2C4059
+          </div>
+          <div className='display'>
+            Five quacking zephyrs jolt my wax bed.
+          </div>
+        </div>
+        <div styleName='style-card'>
+          <div styleName='style-name'>
+            Display
+          </div>
+          <div styleName='style-attributes'>
+            Circular Bold / 32pt, -0.5 ch, 36 line / Color: 2C4059
+          </div>
+          <div className='display'>
+            Five quacking zephyrs jolt my wax bed.
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 }
