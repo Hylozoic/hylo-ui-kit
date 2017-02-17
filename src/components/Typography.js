0a8c05befd896df1791f75fc48258befc4e9806b
Robbie Carlton <robbie.carlton@gmail.com>
2017-02-17 14:31:13 -0700

    final typography clean up

--- src/components/Typography.js
+++ src/components/Typography.js
@@ -3,204 +3,207 @@ import styles from './Typography.css'
 import CSSModules from 'react-css-modules'
 
 function Typography (props) {
-  console.log('styles')
-  return <div styleName='page'>
-    <div styleName='title'>Typography</div>
-    <div styleName='section-title'>Headers</div>
-    <div styleName='section'>
-      <div styleName='row'>
-        <div styleName='style-card'>
-          <div styleName='style-name'>
-            .h-display
-          </div>
-          <div styleName='style-attributes'>
-            Circular Bold / 32pt, -0.5 ch, 36 line / Color: 2C4059
-          </div>
-          <div styleName='style-example-header' className='h-display'>
-            Five quacking zephyrs jolt my wax bed.
-          </div>
-        </div>
-        <div styleName='style-card'>
-          <div styleName='style-name'>
-            .h-subheadline, h2
-          </div>
-          <div styleName='style-attributes'>
-            Circular Book / 18pt, 0 ch, 22 line / Color: 2C4059
-          </div>
-          <div styleName='style-example-header' className='h-subheadline'>
-            Five quacking zephyrs jolt my wax bed.
-          </div>
-        </div>
-      </div>
-      <div styleName='row'>
-        <div styleName='style-card no-border'>
-          <div styleName='style-name'>
-            .h-headline, h1
-          </div>
-          <div styleName='style-attributes'>
-            Circular Medium / 24pt, 0 ch, 28 line / Color: 2C4059
-          </div>
-          <div styleName='style-example-header' className='h-headline'>
-            Five quacking zephyrs jolt my wax bed.
-          </div>
-        </div>
-        <div styleName='style-card no-border'>
-          <div styleName='style-name'>
-            .h-minor, h3
-          </div>
-          <div styleName='style-attributes'>
-            Circular Bold / 14pt, 0 ch, 18 line / Color: 2C4059
-          </div>
-          <div styleName='style-example-header' className='h-minor'>
-            Five quacking zephyrs jolt my wax bed.
-          </div>
-        </div>
-      </div>
-    </div>
-    <div styleName='section-title'>Body Styles</div>
-    <div styleName='section'>
-      <div styleName='row'>
-        <div styleName='style-card'>
-          <div styleName='style-name'>
-            .body-drk-lg
-          </div>
-          <div styleName='style-attributes'>
-            Circular Book / 18pt, 0 ch, 24 line / Color: 2C4059 80%
-          </div>
-          <div styleName='style-example-body' className='body-drk-lg'>
-            Apparently we had reached a great height in the atmosphere, for the sky was a dead black.
-          </div>
-        </div>
-        <div styleName='style-card'>
-          <div styleName='style-name'>
-            .body-drk-sm
-          </div>
-          <div styleName='style-attributes'>
-            Circular Book / 15pt, 0 ch, 22 line / Color: 2C4059 80%
-          </div>
-          <div styleName='style-example-body' className='body-drk-sm'>
-            Apparently we had reached a great height in the atmosphere, for the sky was a dead black.
-          </div>
-        </div>
-      </div>
-      <div styleName='row'>
-        <div styleName='style-card no-border'>
-          <div styleName='style-name'>
-            .body-lt-lg
-          </div>
-          <div styleName='style-attributes'>
-            Circular Book / 17pt, 0 ch, 25 line / Color: 2C4059 60%
-          </div>
-          <div styleName='style-example-body' className='body-lt-lg'>
-            Apparently we had reached a great height in the atmosphere, for the sky was a dead black.
-          </div>
-        </div>
-        <div styleName='style-card no-border'>
-          <div styleName='style-name'>
-            .body-lt-sm
-          </div>
-          <div styleName='style-attributes'>
-            Circular Book / 15pt, 0 ch, 22 line / Color: 2C4059 60%
-          </div>
-          <div styleName='style-example-body' className='body-lt-sm'>
-            Apparently we had reached a great height in the atmosphere, for the sky was a dead black.
-          </div>
-        </div>
-      </div>
-    </div>
-    <div styleName='row'>
-      <div styleName='section-title flex-item left-column'>Captions & Tags</div>
-      <div styleName='section-title flex-item'>Buttons</div>
-    </div>
-    <div styleName='row'>
-      <div styleName='column flex-item left-column'>
-        <div styleName='section'>
+  return <div>
+    <div styleName='sheet'>
+      <div styleName='title'>Typography</div>
+      <div styleName='section-title'>Headers</div>
+      <div styleName='section'>
+        <div styleName='row'>
           <div styleName='style-card'>
             <div styleName='style-name'>
-              Caption 13pt - Black
+              .h-display
             </div>
             <div styleName='style-attributes'>
-              Circular Book / 14pt, 0 ch, 18 line / Color: 2C4059 80%
+              Circular Bold / 32pt, -0.5 ch, 36 line / Color: 2C4059
             </div>
-            <div styleName='style-example-header' className='caption-drk-lg'>
-              Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle.
+            <div styleName='style-example-header' className='h-display'>
+              Five quacking zephyrs jolt my wax bed.
             </div>
           </div>
           <div styleName='style-card'>
             <div styleName='style-name'>
-              Caption 13pt - Grey
+              .h-subheadline, h2
             </div>
             <div styleName='style-attributes'>
-              Circular Book / 13pt, 0 ch, 19 line / Color: 2C4059 60%
+              Circular Book / 18pt, 0 ch, 22 line / Color: 2C4059
             </div>
-            <div styleName='style-example-header' className='caption-lt-lg'>
-              Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle.
+            <div styleName='style-example-header' className='h-subheadline'>
+              Five quacking zephyrs jolt my wax bed.
+            </div>
+          </div>
+        </div>
+        <div styleName='row'>
+          <div styleName='style-card no-border'>
+            <div styleName='style-name'>
+              .h-headline, h1
+            </div>
+            <div styleName='style-attributes'>
+              Circular Medium / 24pt, 0 ch, 28 line / Color: 2C4059
+            </div>
+            <div styleName='style-example-header' className='h-headline'>
+              Five quacking zephyrs jolt my wax bed.
             </div>
           </div>
           <div styleName='style-card no-border'>
             <div styleName='style-name'>
-              Tag
+              .h-minor, h3
             </div>
             <div styleName='style-attributes'>
-              Circular Bold / 10pt, 0.6 ch, 14 line / Color: 8DABC4;
+              Circular Bold / 14pt, 0 ch, 18 line / Color: 2C4059
             </div>
-            <div styleName='style-example-body' className='tag'>
-              DASHBOARDS
+            <div styleName='style-example-header' className='h-minor'>
+              Five quacking zephyrs jolt my wax bed.
             </div>
           </div>
         </div>
       </div>
-      <div styleName='column flex-item'>
-        <div styleName='section'>
+      <div styleName='section-title'>Body Styles</div>
+      <div styleName='section'>
+        <div styleName='row'>
           <div styleName='style-card'>
             <div styleName='style-name'>
-              Button - Regular
+              .body-drk-lg
             </div>
             <div styleName='style-attributes'>
-              Circular Medium / 16pt, 0 ch, 20 line / Color: 0DC3A0
+              Circular Book / 18pt, 0 ch, 24 line / Color: 2C4059 80%
             </div>
-            <div styleName='style-example-body' className='button'>
-              Save changes
+            <div styleName='style-example-body' className='body-drk-lg'>
+              Apparently we had reached a great height in the atmosphere, for the sky was a dead black.
             </div>
           </div>
-          <div styleName='style-card no-border'>
+          <div styleName='style-card'>
             <div styleName='style-name'>
-              Button - Small
+              .body-drk-sm
             </div>
             <div styleName='style-attributes'>
-              Circular Bold / 10pt, 0.6 ch, 14 line / Color: 0DC3A0
+              Circular Book / 15pt, 0 ch, 22 line / Color: 2C4059 80%
             </div>
-            <div styleName='style-example-body' className='button-sm'>
-              Learn more
+            <div styleName='style-example-body' className='body-drk-sm'>
+              Apparently we had reached a great height in the atmosphere, for the sky was a dead black.
             </div>
           </div>
         </div>
-        <div styleName='section-title'>Other</div>
-        <div styleName='section'>
-          <div styleName='style-card'>
+        <div styleName='row'>
+          <div styleName='style-card no-border'>
             <div styleName='style-name'>
-              User Name - Cards
+              .body-lt-lg
             </div>
             <div styleName='style-attributes'>
-              Circular Bold / 14pt, 0 ch, 20 line / Color: 2C4059
+              Circular Book / 17pt, 0 ch, 25 line / Color: 2C4059 60%
             </div>
-            <div styleName='style-example-body' className='cards'>
-              Steven Pham
+            <div styleName='style-example-body' className='body-lt-lg'>
+              Apparently we had reached a great height in the atmosphere, for the sky was a dead black.
             </div>
           </div>
           <div styleName='style-card no-border'>
             <div styleName='style-name'>
-              Timestamp
+              .body-lt-sm
             </div>
             <div styleName='style-attributes'>
-              Circular Book / 13pt, 0 ch, 16 line / Color: 2C4059 60%
+              Circular Book / 15pt, 0 ch, 22 line / Color: 2C4059 60%
             </div>
-            <div styleName='style-example-body' className='timestamp'>
-              6 days ago
+            <div styleName='style-example-body' className='body-lt-sm'>
+              Apparently we had reached a great height in the atmosphere, for the sky was a dead black.
             </div>
           </div>
         </div>
       </div>
+      <div styleName='row'>
+        <div styleName='section-title flex-item left-column'>Captions & Tags</div>
+        <div styleName='section-title flex-item'>Buttons</div>
+      </div>
+      <div styleName='row'>
+        <div styleName='column flex-item left-column'>
+          <div styleName='section'>
+            <div styleName='style-card'>
+              <div styleName='style-name'>
+                Caption 13pt - Black
+              </div>
+              <div styleName='style-attributes'>
+                Circular Book / 14pt, 0 ch, 18 line / Color: 2C4059 80%
+              </div>
+              <div styleName='style-example-header' className='caption-drk-lg'>
+                Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle.
+              </div>
+            </div>
+            <div styleName='style-card'>
+              <div styleName='style-name'>
+                Caption 13pt - Grey
+              </div>
+              <div styleName='style-attributes'>
+                Circular Book / 13pt, 0 ch, 19 line / Color: 2C4059 60%
+              </div>
+              <div styleName='style-example-header' className='caption-lt-lg'>
+                Apparently we had reached a great height in the atmosphere, for the sky was a dead black, and the stars had ceased to twinkle.
+              </div>
+            </div>
+            <div styleName='style-card no-border'>
+              <div styleName='style-name'>
+                Tag
+              </div>
+              <div styleName='style-attributes'>
+                Circular Bold / 10pt, 0.6 ch, 14 line / Color: 8DABC4;
+              </div>
+              <div styleName='style-example-body' className='tag'>
+                DASHBOARDS
+              </div>
+            </div>
+          </div>
+        </div>
+        <div styleName='column flex-item'>
+          <div styleName='section'>
+            <div styleName='style-card'>
+              <div styleName='style-name'>
+                Button - Regular
+              </div>
+              <div styleName='style-attributes'>
+                Circular Medium / 16pt, 0 ch, 20 line / Color: 0DC3A0
+              </div>
+              <div styleName='style-example-body' className='button'>
+                Save changes
+              </div>
+            </div>
+            <div styleName='style-card no-border'>
+              <div styleName='style-name'>
+                Button - Small
+              </div>
+              <div styleName='style-attributes'>
+                Circular Bold / 10pt, 0.6 ch, 14 line / Color: 0DC3A0
+              </div>
+              <div styleName='style-example-body' className='button-sm'>
+                Learn more
+              </div>
+            </div>
+          </div>
+          <div styleName='section-title'>Other</div>
+          <div styleName='section'>
+            <div styleName='style-card'>
+              <div styleName='style-name'>
+                User Name - Cards
+              </div>
+              <div styleName='style-attributes'>
+                Circular Bold / 14pt, 0 ch, 20 line / Color: 2C4059
+              </div>
+              <div styleName='style-example-body' className='cards'>
+                Steven Pham
+              </div>
+            </div>
+            <div styleName='style-card no-border'>
+              <div styleName='style-name'>
+                Timestamp
+              </div>
+              <div styleName='style-attributes'>
+                Circular Book / 13pt, 0 ch, 16 line / Color: 2C4059 60%
+              </div>
+              <div styleName='style-example-body' className='timestamp'>
+                6 days ago
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div styleName='sheet'>
     </div>
   </div>
 }
